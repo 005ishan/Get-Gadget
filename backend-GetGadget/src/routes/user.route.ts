57a888c5bc2d88b44822226e5e1a879cee4f0407
0050505ishan <isan91798@gmail.com>
2026-07-16 15:12:11 +0545

    add auth middleware to unprotected cart routes

--- backend-GetGadget/src/routes/user.route.ts
+++ backend-GetGadget/src/routes/user.route.ts
@@ -5,15 +5,15 @@ import { authorizedMiddleware } from "../middlewares/authorized.middleware";
 const router = Router();
 const controller = new UserController();
 
-router.post("/:userId/favourite", controller.toggleFavourite);
-router.get("/:userId/favourite", controller.getFavourites);
+router.post("/:userId/favourite", authorizedMiddleware, controller.toggleFavourite);
+router.get("/:userId/favourite", authorizedMiddleware, controller.getFavourites);
 
 router.post("/:userId/cart", authorizedMiddleware, controller.addToCart);
-router.put("/:userId/cart", controller.updateCartItem);
-router.delete("/:userId/cart", controller.removeCartItem);
-router.get("/:userId/cart", controller.getCart);
+router.put("/:userId/cart", authorizedMiddleware, controller.updateCartItem);
+router.delete("/:userId/cart", authorizedMiddleware, controller.removeCartItem);
+router.get("/:userId/cart", authorizedMiddleware, controller.getCart);
 router.delete("/:userId/cart/clear", authorizedMiddleware, controller.clearCart);
 
-router.put("/:id",authorizedMiddleware, controller.updateCustomer);
+router.put("/:id", authorizedMiddleware, controller.updateCustomer);
 
 export default router;
